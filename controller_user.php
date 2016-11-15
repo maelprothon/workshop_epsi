81ba6a0378c52c2c979e1badec9de84a72f72d88
Raphaël P <Raphal P>
2016-11-15 17:29:24 +0100

    modification controller user

--- controller_user.php
+++ controller_user.php
@@ -7,24 +7,24 @@ session_start();
 //die(var_dump($_POST));
 if (isset($_POST['action'])) {
     if ($_POST['action'] == "create" && !empty($_POST['name']) && !empty($_POST['firstname'])
-            && !empty($_POST['password']) && !empty($_POST['login']) ) {
+            && !empty($_POST['password']) && !empty($_POST['login']) && !empty($_POST['address']) && !empty($_POST['codepostal']) && !empty($_POST['city']) ) {
         $data = array(
             'name' => $_POST['name'],
             'firstname' => $_POST['firstname'],
             'password' => md5($_POST['password']),
-            'login' => $_POST['login']
+            'login' => $_POST['login'],
+            'address' => $_POST['address'],
+            'codepostal' => $_POST['codepostal'],
+            'city' => $_POST['city']
         );
-        $sql = "INSERT INTO utilisateur(nom, prenom, mdp, login) VALUES(:name, :firstname, :password, :login)";
+        $sql = "INSERT INTO utilisateur(nom, prenom, mdp, login, adresse, codepostale, city) VALUES(:name, :firstname, :password, :login, :address, :codepostal, :city)";
         $response = $bd->query($sql, $data);
-        if ($response)
-            $output['result'] = $response;
-        else
-            $output['error'] = "Erreur de paramètres";
+        $output['result'] = $response;
     }
     if ($_POST['action'] == "connect" && !empty($_POST['login']) && !empty($_POST['password']) ) {
         $data = array(
             'login' => $_POST['login'],
-            'password' => md5($_POST['password']),
+            'password' => md5($_POST['password'])
         );
         $sql = "SELECT * FROM utilisateur WHERE login=:login AND mdp=:password";
         $response = $bd->query($sql, $data);
