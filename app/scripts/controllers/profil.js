3afdb6c0944a16cbc31297dcb0f17717f84af2fd
kodjodogbo <kodjo.dogbo@epsi.fr>
2016-11-17 18:29:59 +0100

    kodjo

--- app/scripts/controllers/profil.js
+++ app/scripts/controllers/profil.js
@@ -8,7 +8,7 @@
  * Controller of the webIhmApp
  */
 angular.module('webIhmApp')
-  .controller('ProfilCtrl', function ($scope, $http) {
+  .controller('ProfilCtrl', function ($scope, $http, $rootScope) {
     console.log('ok');
         var req = {
             method: 'POST',
@@ -18,6 +18,42 @@ angular.module('webIhmApp')
          };
         $http(req)
         .then(function(response) {
-            $scope.produit = response.data.result;
+            $scope.produits = response.data.result;
         });
+        
+      
+      $scope.reserveproduit = function(produit){
+        var data = {
+            action: "CreatePret",
+            id_user_1: $rootScope.loggedUser.id,
+            id_produit: produit.id
+        }
+        var req = {
+            method: 'POST',
+            url: 'http://localhost/workshop_epsi/php/controller_pret.php',
+            data: data,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+         };
+
+         $http(req).then(function (response) {
+                            $scope.message=response;
+                        });    
+    };
+    $scope.voirdetailproduit = function(produit){
+        var data = {
+            action: "getListWithUser",
+            id_user: $rootScope.loggedUser.id,
+        }
+        var req = {
+            method: 'POST',
+            url: 'http://localhost/workshop_epsi/php/controller_pret.php',
+            data: data,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+         };
+
+         $http(req).then(function (response) {
+                            $scope.message=response;
+                        });    
+    };
+    
   });
