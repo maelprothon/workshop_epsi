35a22595f6ffef73fbd2531e241909c527793a29
Charles Bernard <Charles Bernard>
2016-11-17 15:48:17 +0100

    sub/add cagnotte

---
+++ php/controller_categorie.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once 'bd.php';
+$bd = new DB();
+$output = array('error' => '');
+session_start();
+//die(var_dump($request));
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata, true);
+
+if (isset($request['action'])) {
+    if ($request['action'] == "getListCategorie") {
+        $sql = "SELECT * FROM categorie"; 
+        $response = $bd->query($sql, $data);
+        if ($response) {
+            $output['result'] = $response;
+        }
+        else{
+            $output['error'] = "Erreur de r�cup�ration liste";
+        }
+    }
+}
+echo json_encode($output);
