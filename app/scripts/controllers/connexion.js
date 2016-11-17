bb916b01d3c9919e4ff1b8cfd41539b9ca53fd72
Raphaël P <Raphal P>
2016-11-17 15:31:47 +0100

    fix host

--- app/scripts/controllers/connexion.js
+++ app/scripts/controllers/connexion.js
@@ -29,7 +29,7 @@ angular.module('webIhmApp')
     $scope.login = function () {
         var req = {
             method: 'POST',
-            url: 'http://localhost/workshop_epsi/php/controller_user.php',
+            url: 'http://easytroc.prothon.me/workshop_epsi/php/controller_user.php',
             data: $scope.loguser,
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
          };
